9609190754df976297facd988b4b2cd066f47ce5
Roman <4464530+roman8128@users.noreply.github.com>
2025-01-29 12:15:11 +0100

    initial commit

---
+++ src/main/java/com/jsondb/JsondbApplication.java
@@ -0,0 +1,13 @@
+package com.jsondb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsondbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsondbApplication.class, args);
+    }
+
+}
